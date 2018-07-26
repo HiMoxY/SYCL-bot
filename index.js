87cc670be0368225fac67f14365b1f21bde27fd6
HiMoxY <36484725+HiMoxY@users.noreply.github.com>
2018-07-26 02:25:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`.help | By @HiMoxY™#1027 .`,'');
-  console.log('---------------');
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`?mok | by HiMoxY™#7445 |twitch.tv/HiMoxYTM| ${client.guilds.size} Servers`,)
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
-  console.log('---------------')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
-client.on('message', message => {
-     if (message.content === ".servers") {
-     let embed = new Discord.RichEmbed()
-  .setColor("#0000FF")
-  .addField("**Server: **" , client.guilds.size)
-  message.channel.sendEmbed(embed);
-    }
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
@@ -63,7 +75,7 @@ client.on('message', message => {
         message.delete()
     return message.reply(`** No Invite Links :angry: ! **`)
     }
-	if (message.content === '$play') {
+	if (message.content === '?mok') {
     if (message.member.voiceChannel) {       
         message.member.voiceChannel.join()
         .then(connection => {

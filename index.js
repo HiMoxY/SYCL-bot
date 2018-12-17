ee3c9ff45dc4b91e0fd409015f63019537339870
HiMoxY <36484725+HiMoxY@users.noreply.github.com>
2018-12-17 06:01:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on('message', message => {
 });
 client.on('message', msg => {
   if (msg.content === '.invite') {
-    msg.reply('https://discordapp.com/oauth2/authorize/?permissions=805314622&scope=bot&client_id=518083134177280003');
+    msg.reply('https://discordapp.com/oauth2/authorize/?permissions=805314622&scope=bot&client_id=524088162125873153');
   }
 });
 client.on('ready', () => {
